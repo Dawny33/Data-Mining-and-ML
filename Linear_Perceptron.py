1da614436a164cb536161fbcd50464a834972819
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2013-10-05 00:20:10 +0530

    Creating a Linear Perceptron

---
+++ Linear_Perceptron.py
@@ -0,0 +1,2 @@
+#This linear perceptron is a basic example of a neural network.
+#In this code, we use a simple classifier, which involves a threshold for decision making, and is then passed through the heave-side function.
