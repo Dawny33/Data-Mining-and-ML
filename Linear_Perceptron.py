3094baa92a19b30b84007d0b4f70344c5168addc
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2013-10-05 23:31:17 +0530

    Importing some libraries for computation.

--- Linear_Perceptron.py
+++ Linear_Perceptron.py
@@ -1,2 +1,6 @@
 #This linear perceptron is a basic example of a neural network.
 #In this code, we use a simple classifier, which involves a threshold for decision making, and is then passed through the heave-side function.
+
+# Importing libraries
+from random import choice
+from numpy import array, dot, random
