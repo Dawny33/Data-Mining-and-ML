12ffa45e2b4c10dbe06747e1ea3d608691294a90
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2013-11-18 14:21:11 +0530

    Updated the learning rate and the training examples quantity.

--- Linear_Perceptron.py
+++ Linear_Perceptron.py
@@ -8,8 +8,14 @@ from numpy import array, dot, random
 #A simple heave-side (step function) function.
 step = lambda x: 0 if x<1 else 1
 
-#The first two variables are the input variables. and the third one among the triple is the dummy variable.
 #The element inside "[]" and outside the parantheses of the triple is the result of an AND function of the given input elements.
 training_data = [(array([0,0,1]), 0),(array([0,1,1]), 0),(array([1,0,1]), 0),(array([1,1,1]), 1),]
 
 rand = random.rand(3) #We choose three random numbers b|w 0 and 1 for initial weights. 
+
+error = []  #storing errors
+lr = 0.1    #learning rate
+n = 500     #number of training examples
+
+
+
