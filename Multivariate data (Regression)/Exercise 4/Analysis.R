183cfcfe76a88e31049693835d497ecc6314f28e
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-05-26 19:11:28 +0530

    Ex4 updated with R

--- Multivariate data (Regression)/Exercise 4/Analysis.R
+++ Multivariate data (Regression)/Exercise 4/Analysis.R
@@ -1,5 +1,18 @@
 wk = loadWorkbook("D:\\R dir\\first ex\\Data Exercise.xlsx")
-df = readWorksheet(wk, sheet = "Exercise 4")
+df = readWorksheet(wk, sheet = "Sheet 1")
+
 head(df)
 attach(df)
 pairs(df)
+
+
+wk2 = loadWorkbook("D:\\R dir\\first ex\\Data Exercise.xlsx")
+df2 = readWorksheet(wk, sheet = "Exercise 4")
+
+pca <- prcomp(df)
+
+dim(pca$x)
+
+reg <- lm(df2$outcome ~ pca$x[,1] + pca$x[,2] + pca$x[,3] + pca$x[,4] + pca$x[,5])
+
+
