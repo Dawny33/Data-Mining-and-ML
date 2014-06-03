21702d963418f48fcf385bbbbaab49fc37aa7470
Jalem Raj Rohit <rajrohit@iitj.ac.in>
2014-06-03 11:22:50 +0530

    Improved Score
    Technique used: GBM

--- Kaggle/Bike_Demand/Code.R
+++ Kaggle/Bike_Demand/Code.R
@@ -1,12 +1,30 @@
+train <- read.csv("D:/R dir/Kaggle/Bike_Demand/train.csv")
+test <- read.csv("D:/R dir/Kaggle/Bike_Demand/test.csv")
+
 train_df = train[2:12][1:7650,]
 test_df = train[2:12][7651:10886,]
 
 
 
-fit <- lm(count ~ season*temp*atemp + humidity + windspeed + holiday*workingday, data=train_df)
+set.seed(1111)
+genmod<-gbm(count~season*temp*atemp + humidity + windspeed + holiday*workingday
+            ,data=train_df ## registered,casual,count columns
+            ,var.monotone=NULL
+            ,distribution="gaussian"
+            ,n.trees=1200
+            ,shrinkage=0.1
+            ,interaction.depth=3
+            ,bag.fraction = 0.5
+            ,train.fraction = 1
+            ,n.minobsinnode = 10
+            ,cv.folds =10
+            ,keep.data=TRUE
+            ,verbose=TRUE)
 
-pred = predict(fit, test)
+best.iter <- gbm.perf(genmod,method="cv") ##the best iteration number
 
-print(round(pred))
+pred = predict(genmod, test,best.iter,type="response")
 
-write.csv(x =round(pred), file = "Kagggg3.csv",row.names = FALSE)
+
+
+write.csv(x =pred, file = "Kagggg5.csv",row.names = FALSE)
