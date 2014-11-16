43274a93ad491a262c566a4d613c88e238802f47
Dawny33 <rajrohit@iitj.ac.in>
2014-11-16 20:01:12 +0530

    Done with theano matrices

--- Py Deep Learning Basics/basic_tut.py
+++ Py Deep Learning Basics/basic_tut.py
@@ -1,6 +1,7 @@
 import theano.tensor as T
 from theano import function
 from theano import pp
+import numpy
 
 '''
 1. In Theano, all variables must be typed.
@@ -8,16 +9,27 @@ from theano import pp
 3. dscalar is not a class; so X and Y are instances of TheanoVariable class; and they are assigned theano type "scalar".
 '''
 X = T.dscalar("x")
-#print type(X)
+print type(X)
 Y = T.dscalar("y")
 
 z = X+Y
 
 f = function([X,Y], z)
 #print type(f)
-#print f(2,3)
+print f(2,3)
 
 '''
 pp() - Pretty prints the function
 '''
 print pp(z)
+
+'''
+Adding matrices in Theano
+'''
+X = T.dmatrix("x")
+Y = T.dmatrix("y")
+z = X + Y
+f = function([X,Y],z)
+
+print f(numpy.array([[1, 2], [3, 4]]), numpy.array([[10, 20], [30, 40]]))
+
