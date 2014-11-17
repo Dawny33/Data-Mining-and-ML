0c7a4ccc4dbd0e3a1120d1506cac2f4fca393f98
Dawny33 <rajrohit@iitj.ac.in>
2014-11-17 13:02:55 +0530

    Explored dmatrices

--- Py Deep Learning Basics/logistic_and_stuff.py
+++ Py Deep Learning Basics/logistic_and_stuff.py
@@ -8,3 +8,25 @@ Basic Logistic function demo.
 '''
 x = T.dmatrix("x")
 s = 1/(1+T.exp(-x))
+logistic = function([x],s)
+print logistic([[0,1],[-1,-2]])
+#print pp(s)
+
+'''
+Similar to logit function is the tanh
+'''
+s2 = (1+T.tanh(x/2))/2
+tan = function([x], s2)
+print tan([[0,1],[-1,-2]])
+
+'''
+Executing multiple functions
+'''
+a,b = T.dmatrices('a','b')
+diff = a-b
+abs_diff = abs(a-b)
+diff_sq = diff**2
+mult = function([a,b],[diff,abs_diff,diff_sq])
+print mult([[0,1],[1,2]],[[-1,2],[5,7]])
+#print pp(diff)
+#print pp(abs_diff)
